ec7a80632c9e9099bd3b591a98be89d47ea8ccd4
Andreas <andreas.kasa@outlook.com>
2026-03-13 10:10:01 +0100

    Activate main window before async app initialization

--- src/wrath.app/App.xaml.cs
+++ src/wrath.app/App.xaml.cs
@@ -35,23 +35,35 @@ public partial class App : Microsoft.UI.Xaml.Application
             }).Build();
     }
 
-    protected override async void OnLaunched(LaunchActivatedEventArgs args)
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
         _uiScope = _host.Services.CreateScope();
         var logger = _uiScope.ServiceProvider.GetRequiredService<ILogger<App>>();
 
         try
         {
-            var initializer = _uiScope.ServiceProvider.GetRequiredService<SqliteInitializer>();
-            await initializer.InitializeAsync(CancellationToken.None);
-
             _window = _uiScope.ServiceProvider.GetRequiredService<MainWindow>();
             _window.Activate();
+
+            _ = InitializeAsync(logger);
         }
         catch (Exception ex)
         {
             logger.LogCritical(ex, "Application startup failed.");
             throw;
+        }
+    }
+
+    private async Task InitializeAsync(ILogger<App> logger)
+    {
+        try
+        {
+            var initializer = _uiScope!.ServiceProvider.GetRequiredService<SqliteInitializer>();
+            await initializer.InitializeAsync(CancellationToken.None);
+        }
+        catch (Exception ex)
+        {
+            logger.LogCritical(ex, "Background initialization failed.");
         }
     }
 }
